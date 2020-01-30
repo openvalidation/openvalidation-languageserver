df86d1d9c6d31dad32e86b90c7f1274f2ae75dba
Nico Lueg <nicolueg@yahoo.de>
2020-01-30 17:46:53 +0100

    added parameter for jar, that sets the server-port. fixed #17

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,12 @@ import { ChildProcess } from "child_process";
 import { executeJar } from "node-java-connector";
 
 const path = require("path");
+const port = 31057;
 
 export async function startBackend(): Promise<ChildProcess> {
   var relativePath = path.join(path.dirname(__filename), "../java/ov-rest.jar");
 
-  var output = await executeJar(relativePath);
+  var output = await executeJar(relativePath, [`--server.port=${port}`]);
   if (!!output.stderr) {
     output.stderr.on("data", (stderr: any) => {
       console.error(`${stderr}`);
